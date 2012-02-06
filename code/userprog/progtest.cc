670e10d8eae22ceee63ab6a410da40d819241b18
Salem Harrache <salem.harrache@gmail.com>
2012-02-06 14:29:29 +0100

    uncomment fprintf in SynchConsoleTest

--- code/userprog/progtest.cc
+++ code/userprog/progtest.cc
@@ -97,11 +97,12 @@ void ConsoleTest (char *in, char *out)
     }
 }
 
-void SynchConsoleTest (char *in, char *out) {
+void SynchConsoleTest(char *in, char*out)
+{
     char ch;
     SynchConsole *synchconsole = new SynchConsole(in, out);
-    while ((ch = synchconsole->SynchGetChar()) != EOF) {
+    while ( (ch = synchconsole->SynchGetChar()) != EOF )
         synchconsole->SynchPutChar(ch);
-    }
-    //fprintf(stderr, "Solaris: EOF detected in SynchConsole!\n");
+
+    fprintf(stderr, "Solaris: EOF detected in SynchConsole!\n");
 }
