62f0d036537bb8d30c7dcdff99e73de4d1c8c21d
Salem Harrache <salem.harrache@gmail.com>
2012-03-01 19:30:50 +0100

    hide unnecessary compilation messages

--- code/runtest.sh
+++ code/runtest.sh
@@ -3,7 +3,7 @@
 COMMAND='./build-origin/nachos-userprog -rs 1 -x ./build/makethreads'
 
 rm make_error.log 2> /dev/null
-make 2> make_error.log
+make 2> make_error.log 1> /dev/null
 if test ! -e "make_error.log"; then
     cat make_error.log
 else
