7677d57be9dc8f4a6170e8235050f5f599ef9b13
Salem Harrache <salem.harrache@gmail.com>
2012-03-01 18:10:11 +0100

    start to implement UserThread in UserThread class

--- code/userprog/userthread.cc
+++ code/userprog/userthread.cc
@@ -1,27 +1,6 @@
 #include "userthread.h"
 
 
-int do_UserThreadCreate(int f, int arg){
-
-    // On encapsule la fonction et les parametres dans notre structure parameters
-    UserThreadParameters *parameters = new UserThreadParameters;
-    parameters->f = f;
-    parameters->arg = arg;
-
-    Thread* newThread = new Thread((char *)f);
-    if (newThread == NULL){
-        printf("Failed to create new Thread : newThread is NULL\n");
-        return -1;
-    }
-    newThread->Fork(StartUserThread, (int)parameters);
-
-    return (int) newThread;
-}
-
-void do_UserThreadExit() {
-    currentThread->Finish();
-}
-
 void StartUserThread(int parameters){
     UserThreadParameters *p = (UserThreadParameters *) parameters;
     currentThread->space->InitRegisters(p->f, p->arg);
@@ -29,3 +8,36 @@ void StartUserThread(int parameters){
     return;
 }
 
+UserThread::UserThread(const char *debugName, int f, int arg) : Thread(debugName) {
+    // On encapsule la fonction et les parametres dans notre structure parameters
+    this->id = 0;
+    this->parameters = new UserThreadParameters;
+    parameters->f = f;
+    parameters->arg = arg;
+
+}
+
+UserThread::~UserThread() {
+    delete parameters;
+}
+
+void UserThread::StartThread(void) {
+    this->Fork(StartUserThread, (int) this->parameters);
+}
+
+int do_UserThreadCreate(int f, int arg) {
+
+
+    UserThread* newThread = new UserThread("Test", f, arg);
+    if (newThread == NULL) {
+        printf("Failed to create new Thread : newThread is NULL\n");
+        return -1;
+    }
+    newThread->StartThread();
+
+    return (int) newThread;
+}
+
+void do_UserThreadExit() {
+    currentThread->Finish();
+}
--- code/userprog/userthread.h
+++ code/userprog/userthread.h
@@ -5,16 +5,27 @@
 #include "system.h"
 #include "syscall.h"
 
-
 typedef struct {
     int f;
     int arg;
 } UserThreadParameters;
 
+class UserThread : public Thread {
+    public:
+        UserThread(const char *debugName, int f, int arg);
+        UserThreadParameters *parameters;
+        void StartThread(void);
+        ~UserThread();
+        int getId () { return (id); }
+    private:
+        int id;
+#ifdef USER_PROGRAM
+// ...
+#endif
+};
 
 extern int do_UserThreadCreate(int f, int arg);
 extern void StartUserThread(int f);
 extern void do_UserThreadExit();
 
 #endif
-
