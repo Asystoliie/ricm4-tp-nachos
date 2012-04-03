6d6c424ef048f20e1213c704a9861564aee88196
residante <mr.residante@gmail.com>
2012-04-03 12:54:57 +0200

    add some comments

--- code/userprog/userthread.cc
+++ code/userprog/userthread.cc
@@ -5,9 +5,7 @@
 void StartUserThread(int thread) {
     UserThread *t = (UserThread *) thread;
     // L'id du thread informe egalement le numéro de page du thread
-//    IntStatus oldLevel = interrupt->SetLevel (IntOff);
     currentThread->space->InitThreadRegisters(t->func, t->arg, t->getZone());
-//    (void) interrupt->SetLevel (oldLevel);
     currentThread->space->UpdateRunningThreads(1); // appel atomique
     machine->Run();
 }
@@ -15,7 +13,7 @@ void StartUserThread(int thread) {
 UserThread::UserThread(const char *debugName, int f, int a, int callback) : Thread(debugName) {
     this->func = f;
     this->arg = a;
-    // A la fin du thread on appel cette nouvelle fonction
+    // A la fin du thread on appelle cette nouvelle fonction
     this->UpdateCallBackRegister(callback);
 }
 
@@ -36,34 +34,41 @@ int do_UserThreadCreate(int f, int arg, int callback) {
     if (zone < 0) { delete newThread; return 0; }
 
     int thread_id = currentThread->space->GetNewThreadId(zone);
-    if (thread_id < 0) { return 0; }
+    if (thread_id < 0) { return 0; } // on verifie quand meme au cas ou
 
     newThread->setId(thread_id);
     newThread->setZone(zone);
 
     // Avant de commencer on prend le jetton, pour que tout thread qui appelle
     // userThreadJoin sur moi soit bloqué.
+    // Ca permet egalement de ne pas lancer le thread tant que des thread sont
+    // bloque sue le thread precedent qui utilisait cette zone
     currentThread->space->semJoinThreads[newThread->getZone()]->P();
-
+    // Le nouveau thread s'execute sur le meme espace d'adressage que celui
+    // qui fait l'appel systeme
     newThread->Fork();
     return newThread->getId();
 }
 
 void do_UserThreadExit() {
+    // On décremente le nombre de thread en cours d'execution
     currentThread->space->UpdateRunningThreads(-1); // appel atomique
 
     // Je libere les threads en attente sur moi
     currentThread->space->semJoinThreads[currentThread->getZone()]->V();
     // Plusieurs threads peuvent attendre que je me termine.
     // Il faut donc que dans la fonction join, les threads en attente se
-    // reveillent les uns les autres
+    // reveillent les uns les autres (en chaine)
 
     // Mise a jour de la bitmap et de la map {thread-id <-> numéro zone}
+    // On utilise cette structure pour ne pas avoir deux fois le meme numero de thread
     currentThread->space->FreeBitMap(); // appel atomique
     currentThread->space->RemoveId(currentThread->getZone()); // appel atomique
 
     // Si je suis le thread seul/dernier thread, je termine le processus
     if(currentThread->space->Alone()) {
+        // Depuis l'etape 4 on appelle Exit() au lieu de Halt()
+        // interrupt->Halt();
         do_Exit();
     }
     else {
